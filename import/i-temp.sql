87b0a479ba0c93c049696e4725bf5c7879ce18fc
CaiZhiYong <czy.zjnu@gmail.com>
2015-08-05 14:19:41 +0800

    31th week report

--- import/i-temp.sql
+++ import/i-temp.sql
@@ -5,12 +5,12 @@
  */
 
 -- 用于销售周报
-delete from 销售出库单 where date_trunc('month',日期)::date in ('2015-07-01');
-select dataupdate('销售出库单','sale','20150726t');
+delete from 销售出库单 where date_trunc('month',日期)::date in ('2015-07-01', '2015-08-01');
+select dataupdate('销售出库单','sale','20150802t');
 
 -- 用于周报客流
-delete from 零售单客户 where date_trunc('month',日期)::date in ('2015-07-01');
-select dataupdate('零售单客户','retail_c','20150726t');
+delete from 零售单客户 where date_trunc('month',日期)::date in ('2015-07-01', '2015-08-01');
+select dataupdate('零售单客户','retail_c','20150802t');
 
 -- 用于盘点分析
 delete from 盘盈单 where date_trunc('month',日期)::date in ('2015-07-01');
